67e7fdbeeaf8a20f2ed02e6873ffc70c63a3c286
German Lena <german.lena@gmail.com>
2017-01-25 12:46:46 -0300

    multiple bundle names + file name

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # CustomVarLibraryNamePlugin
 
-A WebPack ~~hack~~ plugin to allow to set a different module name to the var `libraryTarget` and the AMD/CommonJS when using `libraryTarget:'umd'`.
+A WebPack ~~hack~~ plugin to allow to set a different module name to the var `libraryTarget` and the AMD/CommonJS when using `libraryTarget:'umd'`. 
+
+The latest relase adds the hability to change the file name and handle multiple bundle names.
 
 ## Install
 
@@ -16,9 +18,28 @@ var CustomVarLibraryNamePlugin = require('webpack-custom-var-library-name-plugin
     plugins: [
         ...
         new CustomVarLibraryNamePlugin({
-          name: 'auth0'
+          name: 'var-name'
         }),
         ...
     ],
 ...
+```
+
+```js
+var CustomVarLibraryNamePlugin = require('webpack-custom-var-library-name-plugin');
+
+    plugins: [
+        ...
+        new CustomVarLibraryNamePlugin({
+          name: {
+            'chunk-name': {
+                var: 'var-name',
+                file: 'file-name'
+            }
+          }
+        }),
+        ...
+    ],
+...
+```
 
--- index.js
+++ index.js
@@ -4,6 +4,27 @@ function CustomVarLibraryNamePlugin(options) {
 
 CustomVarLibraryNamePlugin.prototype.apply = function(compiler) {
   var _this = this;
+  var replaceMap = null;
+
+  if (typeof _this.options.name === 'object') {
+    replaceMap = Object.keys(_this.options.name).reduce(function(prev, key) {
+      if (_this.options.name[key].var) {
+        prev.push({
+          replace: 'root["'+ key +'"]',
+          with: 'root["'+ _this.options.name[key].var +'"]'
+        })
+      }
+
+      if (_this.options.name[key].file) {
+        prev.push({
+          replace: key +'.js',
+          with: _this.options.name[key].file +'.js'
+        })
+      }
+
+      return prev;
+    }, []);
+  }
 
   compiler.plugin("compilation", function(compilation) {
 
@@ -11,7 +32,17 @@ CustomVarLibraryNamePlugin.prototype.apply = function(compiler) {
 
     mainTemplate.plugin('asset-path', function(path, data) {
 
-      return path.indexOf('root[') === 0 ? 'root["' + _this.options.name + '"]' : path;
+      if (replaceMap) {
+        if (data.chunk && data.chunk.name) {
+          path = path.replace('[name]', data.chunk.name);
+        }
+
+        return replaceMap.reduce(function(prev, curr) {
+          return prev.replace(curr.replace, curr.with);
+        }, path);
+      } else {
+        return path.indexOf('root[') === 0 ? 'root["' + _this.options.name + '"]' : path;
+      }
 
     });
   });
